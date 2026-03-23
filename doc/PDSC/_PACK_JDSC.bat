5d246c15c81d13514e73e1e4d56d6a65b4dd17b3
Andrey Osipov <osandr@tut.by>
2026-03-23 19:46:30 +0300

    Добавлено несколько новых компонентов в nRF5_SDK_Drivers и nRF5_SDK_Libraries

--- doc/PDSC/_PACK_JDSC.bat
+++ doc/PDSC/_PACK_JDSC.bat
@@ -1,6 +1,6 @@
 @echo off
-xcopy /Y NordicSemiconductor.nRF5_SDK_Drivers_14.2.0.json ..\..\build\debug\NordicSemiconductor.nRF5_SDK_Drivers_14.2.0\
-xcopy /Y NordicSemiconductor.nRF5_SDK_Libraries_14.2.0.json ..\..\build\debug\NordicSemiconductor.nRF5_SDK_Libraries_14.2.0\
-..\..\utils\7z.exe a -tzip ..\..\build\debug\_NordicSemiconductor.nRF5_SDK_Drivers_14.2.0.jpack -r ..\..\build\debug\NordicSemiconductor.nRF5_SDK_Drivers_14.2.0\*
-..\..\utils\7z.exe a -tzip ..\..\build\debug\_NordicSemiconductor.nRF5_SDK_Libraries_14.2.0.jpack -r ..\..\build\debug\NordicSemiconductor.nRF5_SDK_Libraries_14.2.0\*
+xcopy /Y NordicSemiconductor.nRF5_SDK_Drivers_14.2.0.json ..\..\..\build\debug\NordicSemiconductor.nRF5_SDK_Drivers_14.2.0\
+xcopy /Y NordicSemiconductor.nRF5_SDK_Libraries_14.2.0.json ..\..\..\build\debug\NordicSemiconductor.nRF5_SDK_Libraries_14.2.0\
+..\..\utils\7z.exe a -tzip ..\..\..\build\debug\_NordicSemiconductor.nRF5_SDK_Drivers_14.2.0.jpack -r ..\..\..\build\debug\NordicSemiconductor.nRF5_SDK_Drivers_14.2.0\*
+..\..\utils\7z.exe a -tzip ..\..\..\build\debug\_NordicSemiconductor.nRF5_SDK_Libraries_14.2.0.jpack -r ..\..\..\build\debug\NordicSemiconductor.nRF5_SDK_Libraries_14.2.0\*
 pause
