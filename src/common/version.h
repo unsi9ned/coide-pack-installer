a8bd92c731df3339c3db244dd25a960cf9c4d874
Andrey Osipov <osandr@tut.by>
2026-04-09 13:28:34 +0300

    Обновлена версия релиза v0.1.1 и описания readme.md

--- src/common/version.h
+++ src/common/version.h
@@ -4,15 +4,15 @@
 #pragma once
 
 #define APP_NAME           "CoIDE Pack Installer"
-#define APP_VERSION        "0.1.0"
+#define APP_VERSION        "0.1.1"
 #define APP_VERSION_MAJOR  0
 #define APP_VERSION_MINOR  1
-#define APP_VERSION_PATCH  0
+#define APP_VERSION_PATCH  1
 #define APP_BUILD_DATE     __DATE__
 #define APP_BUILD_TIME     __TIME__
 #define APP_AUTHOR         "Andrey Osipov"
 #define APP_WEBSITE        "https://hamlab.net"
-#define APP_GITHUB         "https://github.com/unsi9ned/"
+#define APP_GITHUB         "https://github.com/unsi9ned/coide-pack-installer"
 
 
 #endif // VERSION_H
